c47752971b30156bdecfdf71e267b46681611fa4
carliehuang <carliehua@gmail.com>
2017-03-26 09:58:36 -0700

    Add files via upload

---
+++ giveaway.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Youtube Connect</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="navbar">
+		<img src="logo.png" id="logo">
+		<nav>
+			<a href="home.php" class="link">home</a>
+			<a href="buy.php" class="link">buy</a>
+			<a href="giveaway.php" class="link">giveaway</a>
+			<a href="login.php" class="link">register/login</a>
+		</nav>
+	</div>
+</body>
+</html>

162273b9cccd185583e9d66e13d5a68780167899
Fiona Thendean <fionahtt@gmail.com>
2017-03-26 09:06:21 -0700

    Update home.php

--- home.php
+++ home.php
@@ -17,9 +17,20 @@
 	</div>
 	<div id="backgroundpic">
 		<!--<img src="background.jpg"/>-->
-
 		      <h2>CONNECT WITH CREATORS</h2>
 			  <h3>CONNECT WITH FANS</h3>
 	</div>
+	</div>
+		<h4>BUY, SELL, CONNECT</h4>
+		<p id="intro"> with some of your favorite YouTubers on YouTube Connect.</p>
+		<p id="body"> YouTube Connect is a platform for YouTubers to sell their used items to their fans and hold giveaways, providing a unique experience for both. Register to learn more. </p>
+		<img id = "casey" src="casey.png"/>
+		<img id="jenn" src="jenn.png"/>
+		<img id = "laurdiy" src="laurdiy.png"/>
+		<img id="mkbhc" src="mkbhc.png"/>
+		<img id = "jenna" src="jenna.png"/>
+		<img id="vanoss" src="vanoss.png"/>
+	</div>
+	
 </body>
 </html>

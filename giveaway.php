8a8cb4a20097caa2d0218cc4252d2495c1ff0e3b
Fiona Thendean <fionahtt@gmail.com>
2017-03-26 10:50:44 -0700

    Update giveaway.php

--- giveaway.php
+++ giveaway.php
@@ -15,5 +15,21 @@
 			<a href="login.php" class="link">register/login</a>
 		</nav>
 	</div>
+	
+	</div>
+		<h5>YOUTUBER GIVEAWAYS</h5>
+		<p id="description"> With YouTube Connect, get notified for important dates and news in giveaways from your favorite YouTubers. Explore the current giveaways below.</p>
+	</div>
+		<img id = "purse" src="purse.jpg"/>
+		<img id = "camera" src="camera.png"/>
+		<img id = "iphone" src="iphone.png"/>
+		<img id = "polaroid" src="polaroid.jpg"/>
+		<img id = "gopro" src="gopro.jpg"/>
+		<img id = "palette" src="palette.jpeg"/>
+		<img id = "computer" src="computer.jpg"/>
+		<img id = "straightener" src="straightener.jpg"/>
+		<img id = "planner" src="planner.jpg"/>
+	</div>
+	</div>
 </body>
 </html>
